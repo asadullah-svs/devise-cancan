40ad4ab9339e8695da5a3c028daa00b91d8c4f1b
zamananjum20 <usama.ashraf.khan@gmail.com>
2019-09-18 12:00:05 +0500

    cancan with devise

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,21 @@ class Ability
   def initialize(user)
 
     if user.admin?
-
+      can :manage, :all
     else
+
+      can :update, Question do |question|
+        question.user == user
+      end
+
+      can :destroy, Question do |question|
+        question.user == user
+      end
+
+      can :destroy, Answer do |answer|
+        answer.user == user
+      end
+
       can :create, Question
       can :create, Answer
     end
